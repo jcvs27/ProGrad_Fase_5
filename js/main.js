6a38a3264d2eb5d2d65270fe6010baa43c48217e
jcvs27 <31634768+jcvs27@users.noreply.github.com>
2020-12-04 21:46:08 -0500

    v8
    Se suben el menú quienes somos

--- js/main.js
+++ js/main.js
@@ -4,24 +4,36 @@ $(document).ready(function(){
     $(".menu_1").show();
     $(".menu_2").hide();
     $(".menu_3").hide();
+    $(".menu_4").hide();
 
     // Para solo mostrar en los trabajo individuales
     $(document).on('click' , '#menu_1', function(){
         $(".menu_1").show();
         $(".menu_2").hide();
         $(".menu_3").hide();
+        $(".menu_4").hide();
     });
     // Para solo mostrar en los trabajo Coloborativo
     $(document).on('click' , '#menu_2', function(){
         $(".menu_1").hide();
         $(".menu_2").show();
         $(".menu_3").hide();
+        $(".menu_4").hide();
     });
     // Para solo mostrar la evaluación
     $(document).on('click' , '#menu_3', function(){
         $(".menu_1").hide();
         $(".menu_2").hide();
         $(".menu_3").show();
+        $(".menu_4").hide();
     });
+
+    // Para solo mostrar Quienes somos
+    $(document).on('click' , '#menu_4', function(){
+        $(".menu_1").hide();
+        $(".menu_2").hide();
+        $(".menu_3").hide();
+        $(".menu_4").show();
+    })
 
 });
