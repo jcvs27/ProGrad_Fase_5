796cdac25cee95c87d41e4f9f978da21621d17c7
jcvs27 <31634768+jcvs27@users.noreply.github.com>
2020-12-04 20:27:00 -0500

    v6
    Se hacen ajuste en los card, se mejora el diseño.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,1 @@
 
-const cards = document.querySelectorAll('.card');
-
-function transition() {
-  if (this.classList.contains('active')) {
-    this.classList.remove('active')
-  } else {
-    this.classList.add('active');
-  }
-}
-
-cards.forEach(card => card.addEventListener('click', transition));
